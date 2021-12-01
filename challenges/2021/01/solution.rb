e18a214b3eca03b6ec814776a90da16c05a8510c
Patrick Kuykendall <patrick@kuykendall.io>
2021-12-01 08:36:16 -0500

    2021-01 Complete

--- challenges/2021/01/solution.rb
+++ challenges/2021/01/solution.rb
@@ -2,11 +2,24 @@
 module Year2021
   class Day01
     def part_1(input)
-      nil
+      numbers = convert_input(input)
+      count_of_increasing(numbers)
     end
 
     def part_2(input)
-      nil
+      numbers = convert_input(input)
+      sums = numbers.each_cons(3).map(&:sum)
+      count_of_increasing(sums)
+    end
+
+    private
+
+    def count_of_increasing(numbers)
+      numbers.each_cons(2).select { |first, second| second > first }.count
+    end
+
+    def convert_input(input)
+      input.split.map(&:to_i)
     end
   end
 end
