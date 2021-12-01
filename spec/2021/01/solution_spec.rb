185d4e1f15a079840fc503d9c99e28134e78104f
Patrick Kuykendall <patrick@kuykendall.io>
2021-12-01 08:36:16 -0500

    2021-01 Complete

--- spec/2021/01/solution_spec.rb
+++ spec/2021/01/solution_spec.rb
@@ -6,28 +6,36 @@ RSpec.describe Year2021::Day01 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), "../../../challenges/2021/01/input.txt")) }
   let(:example_input) {
     <<~EOF
-      change
-      me
+      199
+      200
+      208
+      210
+      200
+      207
+      240
+      269
+      260
+      263
     EOF
   }
 
   describe "part 1" do
-    it "returns nil for the example input" do
-      expect(puzzle.part_1(example_input)).to eq(nil)
+    it "returns 7 for the example input" do
+      expect(puzzle.part_1(example_input)).to eq(7)
     end
 
-    it "returns nil for my input" do
-      expect(puzzle.part_1(input)).to eq(nil)
+    it "returns 1624 for my input" do
+      expect(puzzle.part_1(input)).to eq(1624)
     end
   end
 
   describe "part 2" do
-    it "returns nil for the example input" do
-      expect(puzzle.part_2(example_input)).to eq(nil)
+    it "returns 5 for the example input" do
+      expect(puzzle.part_2(example_input)).to eq(5)
     end
 
-    it "returns nil for my input" do
-      expect(puzzle.part_2(input)).to eq(nil)
+    it "returns 1653 for my input" do
+      expect(puzzle.part_2(input)).to eq(1653)
     end
   end
 end
