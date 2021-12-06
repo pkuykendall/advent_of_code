2384c8a068d44ca15354cea749411e7ab5d34f8d
Patrick Kuykendall <patrick@kuykendall.io>
2021-12-06 09:48:37 -0500

    Completed 2021-4

---
+++ challenges/2021/04/solution.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'set'
+module Year2021
+  class Day04
+    def part_1(input)
+      called_numbers, bingo_sheets = parse_input(input)
+      called_numbers.each.with_object([]) do |num, previous|
+        previous << num
+        winner = bingo_sheets.find do |sheet|
+          clear_matches(num, sheet)
+          break sheet if winning_sheet?(sheet)
+        end
+        break sheet_score(num, winner) if winner
+      end
+    end
+
+    def part_2(input)
+      called_numbers, bingo_sheets = parse_input(input)
+      called_numbers.each do |num|
+        winner = bingo_sheets.find do |sheet|
+          clear_matches(num, sheet)
+          break sheet if bingo_sheets.count == 1 && winning_sheet?(sheet)
+        end
+
+        break sheet_score(num, winner) if winner
+
+        bingo_sheets.reject!(&method(:winning_sheet?))
+      end
+    end
+
+    private
+
+    def sheet_score(num, winner)
+      winner.lines.flatten.sum * num
+    end
+
+    def winning_sheet?(sheet)
+      sheet.lines.any?(&:empty?) || sheet.columns.any?(&:empty?)
+    end
+
+    def clear_matches(num, sheet)
+      sheet.lines.find { |line| line.delete(num) }
+      sheet.columns.find { |col| col.delete(num) }
+    end
+
+    def parse_input(input)
+      split_input    = input.split(/^\n+/)
+      called_numbers = split_input[0].strip.split(',').map!(&:to_i)
+      bingo_sheets   = split_input[1..].map(&method(:build_sheet))
+      [called_numbers, bingo_sheets]
+    end
+
+    def build_sheet(sheet)
+      lines   = sheet.split("\n").map!(&:split)
+      columns = lines.transpose
+
+      OpenStruct.new(
+        lines: lines.map { |line| line.map!(&:to_i) },
+        columns: columns.map { |col| col.map!(&:to_i) }
+      )
+    end
+  end
+end
---
+++ spec/2021/04/solution_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Year2021::Day04 do
+  let(:puzzle) { Year2021::Day04.new }
+  let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2021/04/input.txt')) }
+  let(:example_input) do
+    <<~EOF
+      7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+      22 13 17 11  0
+       8  2 23  4 24
+      21  9 14 16  7
+       6 10  3 18  5
+       1 12 20 15 19
+
+       3 15  0  2 22
+       9 18 13 17  5
+      19  8  7 25 23
+      20 11 10 24  4
+      14 21 16 12  6
+
+      14 21 17 24  4
+      10 16 15  9 19
+      18  8 23 26 20
+      22 11 13  6  5
+       2  0 12  3  7
+    EOF
+  end
+
+  describe 'part 1' do
+    it 'returns 4512 for the example input' do
+      expect(puzzle.part_1(example_input)).to eq(4512)
+    end
+
+    it 'returns 67716 for my input' do
+      expect(puzzle.part_1(input)).to eq(67_716)
+    end
+  end
+
+  describe 'part 2' do
+    it 'returns 1924 for the example input' do
+      expect(puzzle.part_2(example_input)).to eq(1924)
+    end
+
+    it 'returns 1830 for my input' do
+      expect(puzzle.part_2(input)).to eq(1830)
+    end
+  end
+end
