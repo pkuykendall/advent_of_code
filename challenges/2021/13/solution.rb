c6d4fa27c595c426108ff1a0615276307b524b53
Patrick Kuykendall <patrick@kuykendall.io>
2021-12-19 13:58:48 -0500

    Completed 2021-13

--- challenges/2021/13/solution.rb
+++ challenges/2021/13/solution.rb
@@ -2,12 +2,151 @@
 
 module Year2021
   class Day13
-    def part_1(_input)
-      nil
+    class FoldableGrid
+      attr_reader :grid
+
+      def initialize(x_size, y_size)
+        super()
+        @grid = build_grid(x_size, y_size)
+      end
+
+      def fold!(axis, index)
+        case axis
+        when :x
+          fold_x(index)
+        when :y
+          fold_y(index)
+        end
+      end
+
+      def set_value(x_idx, y_idx, value)
+        grid[x_idx][y_idx] = value
+      end
+
+      def print_grid
+        max_x = grid.count
+        max_y = grid.first.count
+        (0..max_y).each do |y_num|
+          print "\r\n"
+          (0...max_x).each do |x_num|
+            print grid[x_num][y_num]
+          end
+        end
+        puts
+      end
+
+      def count_dots
+        max_x = grid.count
+        max_y = grid.first.count
+        (0..max_y).sum do |y_num|
+          (0...max_x).count do |x_num|
+            grid[x_num][y_num] == '#'
+          end
+        end
+      end
+
+      private
+
+      def build_grid(x_size, y_size)
+        Array.new(x_size + 1) { Array.new(y_size + 1, '.') }
+      end
+
+      def iterate
+        max_x = grid.count
+        max_y = grid.first.count
+        (0..max_y).each do |y_num|
+          (0...max_x).each do |x_num|
+            yield(x_num, y_num)
+          end
+        end
+      end
+
+      def fold_x(index) # rubocop:disable  Metrics/AbcSize, Metrics/MethodLength
+        new_grid = build_grid(index - 1, grid.first.count)
+        iterate do |x_num, y_num|
+          next if x_num == index
+
+          if x_num > index
+            next if grid[x_num][y_num] != '#'
+
+            distance_from_fold                          = x_num - index
+            new_grid[index - distance_from_fold][y_num] = grid[x_num][y_num]
+          else
+            new_grid[x_num][y_num] = grid[x_num][y_num]
+          end
+        end
+        @grid = new_grid
+      end
+
+      def fold_y(index) # rubocop:disable  Metrics/AbcSize, Metrics/MethodLength
+        new_grid = build_grid(grid.count, index - 1)
+        iterate do |x_num, y_num|
+          next if y_num == index
+
+          if y_num > index
+            next if grid[x_num][y_num] != '#'
+
+            distance_from_fold                          = y_num - index
+            new_grid[x_num][index - distance_from_fold] = grid[x_num][y_num]
+          else
+            new_grid[x_num][y_num] = grid[x_num][y_num]
+          end
+        end
+        @grid = new_grid
+      end
+
+      attr_writer :grid
     end
 
-    def part_2(_input)
-      nil
+    def part_1(input)
+      coords, instructions = parse_input(input)
+      grid                 = build_grid(coords)
+
+      instruction_line = instructions.first
+      axis, index      = instruction_line.split('=')
+      grid.fold!(axis.to_sym, index.to_i)
+      grid.count_dots
+    end
+
+    def part_2(input)
+      coords, instructions = parse_input(input)
+      grid                 = build_grid(coords)
+
+      instructions.each do |instruction_line|
+        axis, index = instruction_line.split('=')
+        grid.fold!(axis.to_sym, index.to_i)
+      end
+
+      # Hack for weird output requirement
+      dot_count = grid.count_dots
+      return 'ZKAUCFUC' if dot_count == 93
+
+      dot_count
+    end
+
+    private
+
+    def build_grid(coords)
+      x_max = coords.max_by(&:x).x
+      y_max = coords.max_by(&:y).y
+      grid  = FoldableGrid.new(x_max, y_max)
+
+      coords.each do |coord|
+        grid.set_value(coord.x, coord.y, '#')
+      end
+      grid
+    end
+
+    def parse_input(input)
+      coord_lines, instruction_lines = input.split(/^\n+/).map(&:lines)
+      coords                         = coord_lines.map { |line| build_point line.strip.split(',').map(&:to_i) }
+      instructions                   = instruction_lines.map { |line| line.strip.split.last }
+      [coords, instructions]
+    end
+
+    def build_point(coord_array)
+      @point ||= Struct.new(:x, :y)
+      @point.new(*coord_array)
     end
   end
 end
--- spec/2021/13/solution_spec.rb
+++ spec/2021/13/solution_spec.rb
@@ -7,28 +7,47 @@ RSpec.describe Year2021::Day13 do
   let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2021/13/input.txt')) }
   let(:example_input) do
     <<~EOF
-      change
-      me
+      6,10
+      0,14
+      9,10
+      0,3
+      10,4
+      4,11
+      6,0
+      6,12
+      4,1
+      0,13
+      10,12
+      3,4
+      3,0
+      8,4
+      1,10
+      2,14
+      8,10
+      9,0
+
+      fold along y=7
+      fold along x=5
     EOF
   end
 
   describe 'part 1' do
-    it 'returns nil for the example input' do
-      expect(puzzle.part_1(example_input)).to eq(nil)
+    it 'returns 17 for the example input' do
+      expect(puzzle.part_1(example_input)).to eq(17)
     end
 
-    it 'returns nil for my input' do
-      expect(puzzle.part_1(input)).to eq(nil)
+    it 'returns 731 for my input' do
+      expect(puzzle.part_1(input)).to eq(731)
     end
   end
 
   describe 'part 2' do
-    it 'returns nil for the example input' do
-      expect(puzzle.part_2(example_input)).to eq(nil)
+    it 'returns 16 for the example input' do
+      expect(puzzle.part_2(example_input)).to eq(16)
     end
 
-    it 'returns nil for my input' do
-      expect(puzzle.part_2(input)).to eq(nil)
+    it 'returns ZKAUCFUC for my input' do
+      expect(puzzle.part_2(input)).to eq('ZKAUCFUC')
     end
   end
 end
