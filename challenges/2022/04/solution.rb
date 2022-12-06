3cd232cc54b55173c41714faa1e6c07b413a1f70
Patrick Kuykendall <patrick@kuykendall.io>
2022-12-06 14:24:18 -0500

    Completed 2022-4

---
+++ challenges/2022/04/solution.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Year2022
+  class Day04 < Solution
+    def part_1
+      data.count do |section_1, section_2|
+        section_1.cover?(section_2) || section_2.cover?(section_1)
+      end
+    end
+
+    def part_2
+      data.count do |section_1, section_2|
+        smaller, larger = [section_1, section_2].sort_by { |range| range.size }
+        smaller.any? do |element|
+          larger.member?(element)
+        end
+      end
+    end
+
+    private
+
+    # Processes each line of the input file and stores the result in the dataset
+    def process_input(line)
+      line.split(',').map do |section|
+        start, finish = section.split('-').map(&:to_i)
+        (start..finish)
+      end
+    end
+  end
+end
---
+++ spec/2022/04/solution_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Year2022::Day04 do
+  let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2022/04/input.txt')) }
+  let(:example_input) do
+    <<~EOF
+      2-4,6-8
+      2-3,4-5
+      5-7,7-9
+      2-8,3-7
+      6-6,4-6
+      2-6,4-8
+    EOF
+  end
+
+  describe 'part 1' do
+    it 'returns nil for the example input' do
+      expect(described_class.part_1(example_input)).to eq(2)
+    end
+
+    it 'returns nil for my input' do
+      expect(described_class.part_1(input)).to eq(530)
+    end
+  end
+
+  describe 'part 2' do
+    it 'returns nil for the example input' do
+      expect(described_class.part_2(example_input)).to eq(4)
+    end
+
+    it 'returns nil for my input' do
+      expect(described_class.part_2(input)).to eq(903)
+    end
+  end
+end
