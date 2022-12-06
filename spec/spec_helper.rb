cd0cd29b6d0b75e3a89d6c01983b00cf2b0c9351
Patrick Kuykendall <patrick@kuykendall.io>
2022-12-06 08:03:44 -0500

    Updating aoc_rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
-Dir[File.join(File.dirname(__FILE__), '..', 'challenges', '**', '*.rb')].each do |file|
+Dir[File.join(File.dirname(__FILE__), '..', 'challenges', 'shared', '**', '*.rb')].each do |file|
+  require file
+end
+
+Dir[File.join(File.dirname(__FILE__), '..', 'challenges', '20*', '**', '*.rb')].each do |file|
   require file
 end
 
 RSpec.configure do |config|
-  config.formatter = :progress
 end
